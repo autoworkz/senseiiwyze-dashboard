4f76cbd12f6980925064f8df94245202be14fd30
hariscs4997 <48570182+hariscs4997@users.noreply.github.com>
2025-08-28 04:33:59 +0500

    Link Removed

--- src/components/user-dashboard/components/ProgramReadinessView.tsx
+++ src/components/user-dashboard/components/ProgramReadinessView.tsx
@@ -154,12 +154,10 @@ export function ProgramReadinessView({ userId }: { userId: string }) {
                 programRequirements={dashboardData.programRequirements}
             /> */}
             {/* <AdminActions /> */}
-            <Link href={`/app/users/${userId}/`} passHref>
                 <Button disabled className="w-full bg-orange-500 hover:bg-orange-600 text-white flex items-center justify-center">
                     <Eye className="w-5 h-5 mr-2 text-white" />
                     <h3 className="text-md font-semibold text-white">User Analysis Developed From This Data</h3>
                 </Button>
-            </Link>
         </div>
     )
 }
